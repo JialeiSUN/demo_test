108998e2de8416266d32c19f300617f573991c89
JialeiSUN <sun952775@gmail.com>
2021-09-16 13:56:54 +0200

    sujet test 2

---
+++ app/src/main/java/test3/Dictionary.java
@@ -0,0 +1,11 @@
+package test3;
+
+public class Dictionary {
+    private String nameD;
+    public Dictionary(String name){
+        nameD = name;
+    }
+    public String getName(){
+        return this.nameD;
+    }
+}
