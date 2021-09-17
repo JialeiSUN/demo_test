cb452f0db4c14ef1d9bcf21d30fe15e6bc9150db
JialeiSUN <sun952775@gmail.com>
2021-09-17 09:06:41 +0200

    9 - ajouter alltests

---
+++ app/src/test/java/test3/Alltest.java
@@ -0,0 +1,11 @@
+package test3;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+DictionaryTest.class,
+})
+public class Alltest { // Empty class ( introspection )
+
+}
